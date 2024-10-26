472900740ddd129456f3743d68ee29757f888269
VTR <thippareddy1997@gmail.com>
2024-10-26 05:44:03 +0530

    Added Public subnet details

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
+# VPC Details
 resource "aws_vpc" "vpc_info" {
   cidr_block         = var.vpc_info.cidr_block
   enable_dns_support = var.vpc_info.enable_dns_support
   tags               = merge(var.vpc_info.tags, { Name = var.vpc_info.name })
 }
+
+# Creating public subnets
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.vpc_info.id
+  count             = length(var.Public_Subnets)
+  cidr_block        = var.Public_Subnets[count.index].cidr_block
+  availability_zone = var.Public_Subnets[count.index].availability_Zone
+  #tags              = var.Public_Subnets[count.index].tags
+  tags = merge(var.Public_Subnets[count.index].tags, { Name = var.Public_Subnets[count.index].name })
+  depends_on = [aws_vpc.vpc_info]
+
+}
+
--- variable.tf
+++ variable.tf
@@ -1,3 +1,4 @@
+# VPC info
 variable "vpc_info" {
   type = object({
     name               = string
@@ -6,4 +7,15 @@ variable "vpc_info" {
     tags               = map(string)
   })
   description = "This provides vpc info"
+}
+#Public subnet variables
+
+variable "Public_Subnets" {
+  type = list(object({
+    name              = string
+    cidr_block        = string
+    availability_Zone = string
+    tags              = map(string)
+  }))
+  description = "This provide public subnet info"
 }
